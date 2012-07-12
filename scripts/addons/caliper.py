d5670fe0e4419ec531b91b5362f5c3443677010b
macouno <macouno@gmail.com>
2012-07-12 10:33:21 +0000

    I added an extra function to Blender's drivers. It now gets checked on scene update, which is a hack. Hopefully Campbell can come up with a better solution. But for now it works.
    You can currently add a single Empty as the Caliper, and it has a "measurement" property, that has a custom driver function assigned which is auto updated!

--- scripts/addons/caliper.py
+++ scripts/addons/caliper.py
@@ -19,7 +19,7 @@
 bl_info = {
 	"name": "Caliper",
 	"author": "macouno",
-	"version": (0, 1),
+	"version": (2, 1),
 	"blender": (2, 6, 3),
 	"location": "View3D > Add > Caliper",
 	"description": "Add a caliper object",
@@ -29,40 +29,76 @@ bl_info = {
 	"category": "Object"}
 
 import bpy
+from bpy.app.handlers import persistent
 
 # ########################################################
 # By macouno
 # ########################################################
 
+
+
+# DRIVER TO UPDATE THE CALIPERS
+def CaliperUpdate(a):
+	return a*0.5
+
+# LOAD THE CALIPER INTO THE DRIVER NAMESPACE ON FILE LOAD	
+@persistent
+def load_caliper_on_load_file(dummy):
+	bpy.app.driver_namespace['CaliperUpdate'] = CaliperUpdate
+
+# LOAD THE CALIPER INTO THE DRIVER NAMESPACE ON SCENE UPDATE	
+@persistent
+def load_caliper_on_scene_update(dummy):
+	if not bpy.app.driver_namespace.get('CaliperUpdate'):
+		bpy.app.driver_namespace['CaliperUpdate'] = CaliperUpdate
+	
+	
+	
+# FUNCTION TO ADD A CALIPER TO THE SCENE
 class Add_Caliper(bpy.types.Operator):
 	bl_idname = "object.add_caliper"
 	bl_label = "Caliper"
 	bl_options = {'REGISTER', 'UNDO'}
+		
 
 	def execute(self, context):
 		print('adding caliper')
+		
+		# Add an empty
 		bpy.ops.object.add(type='EMPTY', view_align=False, enter_editmode=False, location=(0,0,0), rotation=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+		
+		# Retrieve a link to the new empty
 		caliper = context.active_object
 		caliper.name = 'Caliper'
+		
+		# Add a measurement property to the caliper
+		caliper['measurement'] = 0.0
+		
+		# Add the driver to the measurement so it gets auto updated
+		fcurve = caliper.driver_add('["measurement"]')
+		drv = fcurve.driver
+		drv.type = 'SCRIPTED'
+		drv.expression = 'CaliperUpdate(frame)'
+		drv.show_debug_info = True
+		
 		return {'FINISHED'}
 
 
-# Define "Extras" menu
+# Define menu item
 def menu_func(self, context):
- self.layout.operator(Add_Caliper.bl_idname, icon='PLUGIN')
+	self.layout.operator(Add_Caliper.bl_idname, icon='PLUGIN')
 
-
+# Load and register
 def register():
 	bpy.utils.register_module(__name__)
-
-	# Add "Extras" menu to the "Add Mesh" menu
 	bpy.types.INFO_MT_add.append(menu_func)
+	
+	bpy.app.handlers.load_post.append(load_caliper_on_load_file)
+	bpy.app.handlers.scene_update_pre.append(load_caliper_on_scene_update)
 
-
+# Unregister
 def unregister():
 	bpy.utils.unregister_module(__name__)
-
-	# Remove "Extras" menu from the "Add Mesh" menu.
 	bpy.types.INFO_MT_add.remove(menu_func)
 
 
